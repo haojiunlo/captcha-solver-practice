c17fc037e194f3e3fc2a001716fcffc8e5539f57
haojkijunlo <haojiunlo@gmail.com>
2018-10-15 15:00:54 +0800

    modified

--- train.py
+++ train.py
@@ -7,9 +7,6 @@ import numpy as np
 import torchvision
 import torch.nn.functional as F
 from torchvision import datasets, models, transforms
-import matplotlib
-matplotlib.use("agg")
-import matplotlib.pyplot as plt
 import time
 import os
 import copy
@@ -17,8 +14,6 @@ import copy
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
-    # best_model_wts = copy.deepcopy(model.state_dict())
-    # best_acc = 0.0
 
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
@@ -80,20 +75,21 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(20*9*2, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(1, 16, (5, 5))
+        self.conv2 = nn.Conv2d(16, 32, (5, 5))
+        self.fc1 = nn.Linear(32*9*2, 256)
+        self.fc2 = nn.Linear(256, 10)
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 20*9*2)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return x
+    def forward(self, input_):
+        h1 = F.relu(F.max_pool2d(self.conv1(input_), 2))
+        h1 = F.dropout(h1, p=0.5, training=self.training)
+        h2 = F.relu(F.max_pool2d(self.conv2(h1), 2))
+        h2 = F.dropout(h2, p=0.5, training=self.training)
+        h2 = h2.view(-1, 32*9*2)
+        h3 = F.relu(self.fc1(h2))
+        h3 = F.dropout(h3, p=0.5, training=self.training)
+        h4 = self.fc2(h3)
+        return h4
 
 if __name__ == '__main__':
     data_transforms = {
@@ -117,7 +113,9 @@ if __name__ == '__main__':
     dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
     class_names = image_datasets['train'].classes
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0")
+
+
     model = Net()
     model = model.to(device)
 
@@ -127,5 +125,5 @@ if __name__ == '__main__':
     optimizer = optim.Adam(model.parameters())
 
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
-    model = train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs=50)
+    model = train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs=40)
     torch.save(model, 'torch_model.pkl')
